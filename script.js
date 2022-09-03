201cf6672c7399a3cfb3db91165dd2ca35ee0d0d
Wiktor-Siemiatkowski <wiktor.siemiatkowski20@gmail.com>
2022-09-03 21:07:37 +0200

    Add game function and polish

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 // Get random int in range 1-3 from computer, assign that int to a variable for later comaprison.
-let compChoice = 0;
+let compChoice = null;
 
 function getComputerChoice () {
     min = 1;
     max = 3;
-    return Math.floor(Math.random() * (max - min + 1) + min);
+    return compChoice = Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-compChoice = getComputerChoice();
-
-console.log(compChoice);
 //Get user choice of rock paper or scissors
-let userChoice = prompt("Choose Rock, Paper or Scissors: ");
+
+let userChoice = null;
+
+function getUserChoice() {
+userChoice = prompt("Choose Rock, Paper or Scissors: ");
 
 if (userChoice.toLowerCase() === 'rock') {
     userChoice = 1
@@ -20,20 +21,47 @@ if (userChoice.toLowerCase() === 'rock') {
 } else if (userChoice.toLowerCase() === 'scissors') {
     userChoice = 3
 } else {
-    alert('Please select a right choice.')
+    console.log('Please select a right choice.')
 };
+}
 
-console.log(userChoice);
+//Score variables
+
+let compScore = 0;
+let userScore = 0;
 
 //Comparison function, listing all possible ties, wins and loses.
-function playOneround () {
+function playOneRound () {
+
+    getComputerChoice();
+    getUserChoice();
+
     if (compChoice === userChoice) {
-        alert('It\'s a tie!')
+        console.log('It\'s a tie!')
     } else if (compChoice === 1 && userChoice === 2 || compChoice === 2 && userChoice === 3 || compChoice === 3 && userChoice === 1) {
-        alert('You have won!')
+        console.log('You have won!')
+        userScore++
     } else if (compChoice === 1 && userChoice === 3 || compChoice === 2 && userChoice === 1 || compChoice === 3 && userChoice === 2) {
-        alert('You have lost!')
+        console.log('You have lost!')
+        compScore++
     }
 }
 
-playOneround();
+//Loop for more rounds
+
+function game() {
+    while (userScore < 5 && compScore < 5) {
+        playOneRound();
+        console.log('Your score is: ' + userScore);
+        console.log('Computer score is: ' + compScore);
+    }
+
+    if (userScore === 5) {
+        console.log('You have won the game!')
+    } else if (compScore === 5) {
+        console.log('Sorry, you have lost the game!')
+    }
+}
+
+game();
+
