b688ddb860aab3419016b5a51f620372777631cd
Wiktor-Siemiatkowski <wiktor.siemiatkowski20@gmail.com>
2022-09-07 13:33:42 +0200

    Add gameplay and score

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let compChoice = null;
 let userChoice = null;
 let compScore = 0;
 let userScore = 0;
+const outcomemsg = document.querySelector('.outcome')
 const showScore = document.querySelector('.score');
 const choiceRock = document.querySelector('.rock');
 const choicePaper = document.querySelector('.paper');
@@ -16,21 +17,34 @@ function getComputerChoice () {
     return compChoice = Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function getWinner() {
-    userChoice = e.target.class;
+function playOneRound () {
+
     getComputerChoice();
-    if (compChoice == userChoice) {
-        console.log('Tie');
-    }}
+
+    if (compChoice === userChoice) {
+        outcomemsg.textContent = 'It\'s a tie!'
+    } else if (compChoice === 1 && userChoice === 2 || compChoice === 2 && userChoice === 3 || compChoice === 3 && userChoice === 1) {
+        outcomemsg.textContent = 'You\'ve won!'
+        userScore++
+    } else if (compChoice === 1 && userChoice === 3 || compChoice === 2 && userChoice === 1 || compChoice === 3 && userChoice === 2) {
+        outcomemsg.textContent = 'You\'ve lost!'
+        compScore++
+    }
+    showScore.textContent = `Your score --> ${userScore} ${compScore} <-- AI score`;
+}
 
 choiceRock.addEventListener('click', () => {
     userChoice = 1;
+    playOneRound();
 })
 
 choicePaper.addEventListener('click', () => {
     userChoice = 2;
+    playOneRound();
 })
 
 choiceScissors.addEventListener('click', () => {
     userChoice = 3;
+    playOneRound();
 })
+
